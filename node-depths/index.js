fc39819f537dc3003f8c0822a260b81a4b7051fc
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-11-29 15:28:56 -0800

    solved with recursion

---
+++ node-depths/index.js
@@ -0,0 +1,16 @@
+class BinaryTree {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+// solved recursively with 0(n)
+function nodeDepths(root, depth = 0) {
+    // create running 'depth' variable with initial 0 value
+    // if the either the left or right node doesn't exist (if node only has one/either child), escape
+    if (!root) return 0
+    // recursively traverse with DFS for node's left and right children, incremeneting each node's depth by re-assigning it to 'depth' and return the result
+    return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1)
+}

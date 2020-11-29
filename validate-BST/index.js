85f0be5c38b96da370a2512fc9e0ed762616ba60
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-11-28 22:57:48 -0800

    added validation-BST solution

---
+++ validate-BST/index.js
@@ -0,0 +1,21 @@
+class BST {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+// solved with 0(n)
+function validateBst(tree, min, max) {
+    // if node is a leaf ie has no children ie is at the bottom, return true
+    if (tree === null) return true
+    // if node is less than min or greater than or equal to max, return false
+    if (tree.value < min || tree.value >= max) return false
+    // recurse through all left-sides of nodes and if invalid, return false
+    if (!validateBst(tree.left, min, tree.value)) return false
+    // recurse through all right-sides of nodes and if invalid, return false
+    if (!validateBst(tree.right, tree.value, max)) return false
+    // if successfully escapes all recursive validations, return true
+    return true
+}

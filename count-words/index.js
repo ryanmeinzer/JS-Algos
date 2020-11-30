0ad69fa926f5dd0d40865f09ce6049cfae688b90
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-11-30 12:05:04 -0800

    almost there with just regex

--- count-words/index.js
+++ count-words/index.js
@@ -6,23 +6,34 @@ if (howMany(sentence) === answer) {
 } else {
     console.log('failed')
 }
+console.log(howMany(sentence))
 
-// take two with just regex
+// take three with simpler regex
 function howMany(sentence) {
-    // create counter variable
-    let counter = 0
-    // create 'checkArr' array for all elements of sentence split by a space
-    let checkArr = [sentence.split(' ').join()]
-    console.log(checkArr)
-    // loop through all elements of 'checkArr'
-    for (let i = 0; i < checkArr.length - 1; i++) {
-        // for (let i of checkArr){
-        // determine if the element has an alphanumeric character inside of it
-        if (i.match(/[a-z]/gi)) {
-            // if it does, increment counter
-            counter++
-        }
-    }
-    // return counter
-    return counter
+    // USSR
+    // create results variable to hold result
+    // globally counting only words and with a hyphen
+    let result = sentence.match(/[\w\-]+/gi)
+    // return result if not null and 0 if null
+    return result ? result.length : 0
 }
+
+// // take two with just regex
+// function howMany(sentence) {
+//     // create counter variable
+//     let counter = 0
+//     // create 'checkArr' array for all elements of sentence split by a space
+//     let checkArr = [sentence.split(' ').join()]
+//     console.log(checkArr)
+//     // loop through all elements of 'checkArr'
+//     for (let i = 0; i < checkArr.length - 1; i++) {
+//         // for (let i of checkArr){
+//         // determine if the element has an alphanumeric character inside of it
+//         if (i.match(/[a-z]/gi)) {
+//             // if it does, increment counter
+//             counter++
+//         }
+//     }
+//     // return counter
+//     return counter
+// }

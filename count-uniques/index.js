6697da7116b4e24c8b914181a368aa30d9b1650c
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-12-05 18:23:03 -0800

    solved with two pointers with 0(n)

---
+++ count-uniques/index.js
@@ -0,0 +1,25 @@
+// test
+let array = [1,1,2,3,3,4,5,5,7,8,10]
+let answer = 8
+if (countUniqueValues(array) === answer) {
+    console.log('passed')
+} else {
+    console.log('failed')
+}
+
+// count the unique values in a sorted array
+// solved with two pointers with 0(n)
+function countUniqueValues(array) {
+    // use two pointers 'i' and 'j' starting at first two elements of array, with i starting at index of 0 and j starting at index of 1
+    let i = 0
+    for (let j = 1; j < array.length; j++) {
+        // if index of i is different than index of j, increment i and replace index of i with index of j
+        if (array[i] !== array[j]) {
+            i++
+            array[i] = array[j]
+        }
+        // else increment j if the same with loop
+    }
+    // index of i + 1 is the number of unique elements
+    return i + 1
+}

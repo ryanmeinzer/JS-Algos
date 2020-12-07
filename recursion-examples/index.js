7097d7b9958920c6050ed3dcd15c3670250ddff1
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-12-06 20:22:11 -0800

    solved with pure recursion

--- recursion-examples/index.js
+++ recursion-examples/index.js
@@ -1,33 +1,72 @@
-// test
+// // tests for 1/3:
+// let n = 5
+// countDown(n)
+
+// // tests for 2/3:
+// let n = 5
+// let answer = 15
+// if (sumRange(n) === answer) {
+//     console.log('passed')
+// } else {
+//     console.log('failed')
+// } 
+
+// tests for 3/3:
 let n = 5
-let answer = 15
-if (sumRange(n) === answer) {
+let answer = 120
+if (factorial(n) === answer) {
     console.log('passed')
 } else {
     console.log('failed')
 } 
 
-// example 2/2 - write a function with iteration then recursion that sums all integers from 0 to n
+// example 3/3 - write a funcdtion with iteration and recursion that multiplies all integers from 0 to n
 
 // recursion
-function sumRange(n) {
-    // base case
-    if (n === 1) return 1
-    // different input by decrementing n
-    // recurse
-    return n + sumRange(n - 1)
+function factorial(n) {
+    // base case and exit recursion
+    if (n === 1) return 1 
+    // recurse with different inputs
+    return n * factorial(n - 1)
 }
 
-// iteration
-function sumRange(n) {
-    let tempSum = 0
-    for (let i = n; i > 0; i--) {
-        tempSum = i + tempSum
-    }
-    return tempSum
-}
 
-// example 1/2 - write a function with iteration then recursion that counts down numbers
+// // iteration
+// function factorial(n) {
+//     let total = 1
+//     // iterate through with loop
+//     for (let i = n; i > 0; i--) {
+//         total *= i
+//     }
+//     // once exit loop 
+//     return total
+// }
+
+// example 2/3 - write a function with iteration then recursion that sums all integers from 0 to n
+
+// // recursion
+// function sumRange(n) {
+//     // base case and exit
+//     if (n === 1) return 1
+//     // different input by decrementing n
+//     // recurse
+//     return n + sumRange(n - 1)
+// }
+
+// // iteration
+// function sumRange(n) {
+//     let tempSum = 0
+//     // iterate through with loop
+//     for (let i = n; i > 0; i--) {
+//         tempSum += i
+//         // same as:
+//         // tempSum = i + tempSum
+//     }
+//     // once exits loop
+//     return tempSum
+// }
+
+// example 1/3 - write a function with iteration then recursion that counts down numbers
 
 // // recursion
 // function countDown(n) {
@@ -40,11 +79,12 @@ function sumRange(n) {
 //     // different input by decrementing n 
 //     n--
 //     // recurse
-//     countDown(n)
+//     return countDown(n)
 // }
 
 // // iteration
 // function countDown(n) {
+//     // iterate through with loop
 //     for (let i = n; i > 0; i--) {
 //         console.log(i)
 //     }

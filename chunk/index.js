cdaca2eced14fac4f8a361b8677b8ee052bf9f14
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-12-07 16:48:00 -0800

    update problem statement

--- chunk/index.js
+++ chunk/index.js
@@ -1,13 +1,3 @@
-// --- Directions
-// Given an array and chunk size, divide the array into many subarrays
-// where each subarray is of length size
-// --- Examples
-// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
-// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
-// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
-// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
-// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
-// test
 let array = [1, 2, 3, 4, 5]
 let size = 4
 let answer = [ [ 1, 2, 3, 4 ], [ 5 ] ]
@@ -16,7 +6,6 @@ if (JSON.stringify(chunk(array, size)) === JSON.stringify(answer)) {
 } else {
     console.log('failed')
 }
-
 
 // return 'chunked' subarrays of given length size
 

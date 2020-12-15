d881438ee56141efbdb9fd4a9bc88be869da6468
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-12-15 09:59:02 -0800

    // return the count of words in a sentence, with a hyphenated word counting as a single word and with numbers not counted as a word

--- count-words/index.js
+++ count-words/index.js
@@ -8,7 +8,7 @@ if (howMany(sentence) === answer) {
 }
 // console.log(howMany(sentence))
 
-// return the count of words in a sentence, with a hyphenated word counting as a single word and with numbers not counted as a word
+// return the count of words in a sentence, with a hyphenated word counting as a single word and with numbers not counted as a word 
 
 // take three (solved) with simpler regex
 function howMany(sentence) {

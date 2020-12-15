3386bfdcb658911f4ec603a78cd49d9a2adb958f
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-12-15 10:12:45 -0800

    // given an array of distinct integers, return any two numbers that equal the target sum

--- two-number-sum/index.js
+++ two-number-sum/index.js
@@ -10,7 +10,7 @@ if (twoNumberSum(array, targetSum).toString() === answer.toString()) {
     console.log('failed')
 } 
 
-// given an array of distinct integers, return any two numbers that equal the target sum
+// given an array of distinct integers, return any two numbers that equal the target sum 
 
 // // solved with two for loops with 0(n ^ 2)
 // function twoNumberSum(array, targetSum) {

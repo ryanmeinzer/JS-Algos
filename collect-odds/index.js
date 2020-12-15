aae057510e3199a856edd31e93637549610f99d4
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-12-15 09:58:28 -0800

    // return all odd values of an array

--- collect-odds/index.js
+++ collect-odds/index.js
@@ -9,7 +9,7 @@ if (collectOddValues(arr).toString() === answer.toString()) {
     console.log('failed')
 }
 
-// return all odd values of an array
+// return all odd values of an array 
 
 // solved with pure recursion
 function collectOddValues(arr) {

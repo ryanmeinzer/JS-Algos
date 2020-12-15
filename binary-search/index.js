659145cd48e965bb7aaa20ff2346523dc506ee17
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-12-15 10:37:37 -0800

    solved with linear search to show differences

--- binary-search/index.js
+++ binary-search/index.js
@@ -9,28 +9,28 @@ if (binarySearch(array, target) === 3) {
     console.log('failed')
 } 
 
-// using BS, return the index of a target integer within an array, or -1 if not found 
+// using BS, find and return the index of a target integer within an array, or -1 if not found 
 
-// solved with iteration with  runtime
-function binarySearch(array, target, left = 0, right = array.length - 1) {
-    // initialize a 'left' index for first element of array
-    // initialize a 'right' index for the last element of array
-    // iterate - while we are not at the base case, is the left greater than right, if so return -1 as target doesn't exist in array
-    while (left <= right) {
-        // find middle number of array
-        // calculate the 'middle' index as the average of the 'left' and 'right' rounded down
-        let middle = Math.floor((left + right) / 2)
-        // create 'middle' value variable
-        let middleValue = array[middle]
-        // if 'target' is equal to 'middle', return middle index
-        if (target === middleValue) return middle
-        // if 'target' is less than 'middle', discard all to the right by re-assigning 'right' to 'middle' - 1
-        if (target < middleValue) right = middle - 1
-        // if 'target' is greater than 'middle', discard all to the left by re-assigning 'left' to 'middle' + 1
-        if (target > middleValue) left = middle + 1
-    }
-    return -1
-}
+// // solved with iteration with 0(log n) runtime
+// function binarySearch(array, target, left = 0, right = array.length - 1) {
+//     // initialize a 'left' index for first element of array
+//     // initialize a 'right' index for the last element of array
+//     // iterate - while we are not at the base case, is the left greater than right, if so return -1 as target doesn't exist in array
+//     while (left <= right) {
+//         // find middle number of array
+//         // calculate the 'middle' index as the average of the 'left' and 'right' rounded down
+//         let middle = Math.floor((left + right) / 2)
+//         // create 'middle' value variable
+//         let middleValue = array[middle]
+//         // if 'target' is equal to 'middle', return middle index
+//         if (target === middleValue) return middle
+//         // if 'target' is less than 'middle', discard all to the right by re-assigning 'right' to 'middle' - 1
+//         if (target < middleValue) right = middle - 1
+//         // if 'target' is greater than 'middle', discard all to the left by re-assigning 'left' to 'middle' + 1
+//         if (target > middleValue) left = middle + 1
+//     }
+//     return -1
+// }
 
 // // solved with recursion with 0(log n) runtime
 // function binarySearch(array, target, left = 0, right = array.length - 1) {
@@ -50,3 +50,13 @@ function binarySearch(array, target, left = 0, right = array.length - 1) {
 //     // recurse - if 'target' is greater than 'middle', discard all to the left by re-assigning 'left' to 'middle' + 1
 //     if (target > middleValue) return binarySearch(array, target, middle + 1, right)
 // }
+
+// solved with linear search (not BS) with 0(n)
+function binarySearch(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return i
+        }
+    }
+    return -1
+}

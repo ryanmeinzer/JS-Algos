3dcccfd2daa41ae9af330b9ec695e46ac1de13d1
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-01-05 15:22:02 -0800

    // return a merge-sorted array from an array of integers

--- merge-sort/index.js
+++ merge-sort/index.js
@@ -52,3 +52,34 @@ function merge(left, right) {
     // results.push(...right)
     // return results
 }
+
+// // solved with multiple helper functions in similar fashion:
+// // find and removes the smallest element in a sorted array
+// function findMinAndRemoveSorted(array) {
+//     return array.shift()
+// }
+// // combine two sorted arrays into one larger array
+// function merge(firstSubarray, secondSubArray) {
+//     let sorted = []
+//     while (firstSubarray.length != 0 && secondSubArray.length != 0) {
+//         if (firstSubarray[0] < secondSubArray[0]) {
+//             sorted.push(findMinAndRemoveSorted(firstSubarray))
+//         } else {
+//             sorted.push(findMinAndRemoveSorted(secondSubArray))
+//         }
+//     }
+//     return sorted.concat(firstSubarray).concat(secondSubArray)
+// }
+// // mergeSort
+// function mergeSort(array) {
+//     let midpoint = array.length / 2
+//     let firstHalf = array.slice(0, midpoint)
+//     let secondHalf = array.slice(midpoint, array.length)
+//     let sorted
+//     if (array.length < 2) {
+//         return array
+//     } else {
+//         sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+//     }
+//     return sorted
+// }

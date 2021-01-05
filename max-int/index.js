6ac55bfc199f10b64805639f51961df81a469a01
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-01-05 00:57:23 -0800

    // return the max integer of an array

---
+++ max-int/index.js
@@ -0,0 +1,15 @@
+let arr = [1, 4, 6, 3, 8]
+let answer = 8
+if (maxOf(str) === answer) {
+    console.log('passed')
+} else {
+    console.log('failed')
+}
+
+// return the max integer of an array
+function maxOf(arr) {
+    // base case and exit
+    if (arr.length === 1) return arr[0]
+    // change input by recursion
+    return Math.max(arr.pop(), maxOf(arr))
+}

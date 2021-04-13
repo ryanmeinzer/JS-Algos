50aaa2b1c6981ce55a2db1e547e119059286a19e
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-04-12 23:22:20 -0700

    rate-limiter problem solved with array and hash

---
+++ rate-limiter/rateLimiterHash.js
@@ -0,0 +1,38 @@
+let logs = {}
+
+let tenthIndexWindow = logs[Object.keys(logs).length - 1] - logs[Object.keys(logs).length - 11]
+
+function sayHello() {
+    let curTime = new Date()
+    logs[Object.keys(logs).length] = curTime
+    // logs.push(curTime)
+    console.log('hello world', `- invocation #${Object.keys(logs).length} at ${tenthIndexWindow || logs[Object.keys(logs).length - 1] - logs[0]}ms`)
+    rateLimiter()
+}
+
+function rateLimiter() {
+    // console.time('sayHello')
+    if (Object.keys(logs).length < 11 || tenthIndexWindow >= 1000) {
+    // console.timeEnd('sayHello')
+        return console.log('invocation within bounds')
+    } else {
+        throw new Error(`invocation out of bounds`)
+    }
+}
+
+const firstSecondTest = () => {
+    setTimeout(sayHello, 100)
+    setTimeout(sayHello, 200)
+    setTimeout(sayHello, 300)
+    setTimeout(sayHello, 400)
+    setTimeout(sayHello, 500)
+    setTimeout(sayHello, 600)
+    setTimeout(sayHello, 700)
+    setTimeout(sayHello, 800)
+    setTimeout(sayHello, 900)
+    setTimeout(sayHello, 950)
+    // out of bounds
+    setTimeout(sayHello, 1000)
+}
+
+firstSecondTest()
